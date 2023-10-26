84e0ba24d8a300211831fce55f2b871934a1bb38
MGEternal <gmgame1020@outlook.com>
2023-10-26 23:15:25 +0700

    responsive Desktop Ipad Iphone

--- Projectwebapp/ClientApp/src/component/MyNav.js
+++ Projectwebapp/ClientApp/src/component/MyNav.js
@@ -8,9 +8,9 @@ function MyNav() {
             <div class="navbar">
                 <div class="logo"><a href='/'>GrabME</a></div>
                 <ul className='ul-text'>
-                    <li><a href="/" className={path == "/" ? 'active-nav' : ''}>Requests </a></li>
-                    <li><a href="accept" className={path == "/accept" ? 'active-nav' : ''}>Accepted</a></li>
-                    <li><a href="order" className={path == "/order" ? 'active-nav' : ''}>Post</a></li>
+                    <li><a href="/" className={path === "/" ? 'active-nav' : ''}>Requests </a></li>
+                    <li><a href="accept" className={path === "/accept" ? 'active-nav' : ''}>Accepted</a></li>
+                    <li><a href="order" className={path === "/order" ? 'active-nav' : ''}>Post</a></li>
                 </ul>
             </div>
         </navbar>
